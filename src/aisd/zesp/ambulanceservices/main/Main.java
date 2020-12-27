6834043315098a2b440b89dbf10c312dc7169e74
scintilla4evr <kacper17@outlook.com>
2020-12-27 16:20:31 +0100

    Project file structure and basic JavaFX setup.

---
+++ src/aisd/zesp/ambulanceservices/main/Main.java
@@ -0,0 +1,18 @@
+package aisd.zesp.ambulanceservices.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // ...
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
