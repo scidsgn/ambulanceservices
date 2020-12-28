074ecf02d0045eee7e126533385560c015b02861
scintilla4evr <kacper17@outlook.com>
2020-12-28 16:28:42 +0100

    Added Graph.finalizeNodes(), Graph.getLength(), Graph.getMark().

--- src/aisd/zesp/ambulanceservices/graph/Graph.java
+++ src/aisd/zesp/ambulanceservices/graph/Graph.java
@@ -1,14 +1,19 @@
 package aisd.zesp.ambulanceservices.graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Graph<T> {
     private Double[][] edges = null;
-    private Boolean[] marks = null;
+    private boolean[] marks = null;
     private List<T> nodes = new ArrayList<>();
 
     public void addNode(T node) throws IllegalArgumentException, NullPointerException {
+        if (edges != null) {
+            throw new NullPointerException("Cannot add more nodes after finalizing them.");
+        }
+
         if (node == null) {
             throw new NullPointerException("Graph node can't be null.");
         }
@@ -26,12 +31,51 @@ public class Graph<T> {
 
         int n1Index = nodes.indexOf(n1);
         int n2Index = nodes.indexOf(n2);
-
         if (n1Index == -1 || n2Index == -1) {
-            throw new IllegalArgumentException("Node(s) must be added to the graph first.");
+            throw new IllegalArgumentException("Node(s) are not present in the graph.");
         }
 
         edges[n1Index][n2Index] = length;
         edges[n2Index][n1Index] = length;
+    }
+
+    public void finalizeNodes() throws IllegalArgumentException {
+        if (nodes.size() == 0) {
+            throw new IllegalArgumentException("Cannot finalize an empty graph.");
+        }
+
+        int n = nodes.size();
+
+        edges = new Double[n][n];
+        marks = new boolean[n];
+
+        Arrays.fill(marks, false);
+    }
+
+    public Double getLength(T n1, T n2) throws IllegalArgumentException, NullPointerException {
+        if (edges == null) {
+            throw new NullPointerException("Nodes must be finalized first.");
+        }
+
+        int n1Index = nodes.indexOf(n1);
+        int n2Index = nodes.indexOf(n2);
+        if (n1Index == -1 || n2Index == -1) {
+            throw new IllegalArgumentException("Node(s) are not present in the graph.");
+        }
+
+        return edges[n1Index][n2Index];
+    }
+
+    public boolean getMark(T node) throws IllegalArgumentException, NullPointerException {
+        if (edges == null) {
+            throw new NullPointerException("Nodes must be finalized first.");
+        }
+
+        int nIndex = nodes.indexOf(node);
+        if (nIndex == -1) {
+            throw new IllegalArgumentException("Node is not present in the graph.");
+        }
+
+        return marks[nIndex];
     }
 }
