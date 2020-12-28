10e2c3ad84be629884019121925b652d06784fe0
scintilla4evr <kacper17@outlook.com>
2020-12-28 20:06:50 +0100

    Added DijkstraAlgorithm.getPath()

--- src/aisd/zesp/ambulanceservices/graph/DijkstraAlgorithm.java
+++ src/aisd/zesp/ambulanceservices/graph/DijkstraAlgorithm.java
@@ -15,4 +15,27 @@ public class DijkstraAlgorithm<T> {
         distances = new Double[n];
         previousNodes = new ArrayList<T>(n);
     }
+
+    public List<T> getPath(T target) {
+        List<T> nodes = graph.getNodes();
+
+        int nIndex = nodes.indexOf(target);
+        if (nIndex == -1) {
+            throw new IllegalArgumentException("Node is not present in the graph.");
+        }
+
+        if (previousNodes.get(nIndex) == null) {
+            return null;
+        }
+
+        List<T> path = new ArrayList<>();
+
+        do {
+            path.add(nodes.get(nIndex));
+
+            nIndex = nodes.indexOf(previousNodes.get(nIndex));
+        } while (nIndex != -1);
+
+        return path;
+    }
 }
