d268d2429cd97916f2451ba1953f277942eb111c
Achreko <achrekv3@gmail.com>
2020-12-31 01:40:04 +0100

    Points are made!

--- src/aisd/zesp/ambulanceservices/geometry/LineIntersection.java
+++ src/aisd/zesp/ambulanceservices/geometry/LineIntersection.java
@@ -1,4 +1,10 @@
 package aisd.zesp.ambulanceservices.geometry;
 
 public class LineIntersection {
+    public double intersect(Point start, Point end){
+        double ratio;
+
+
+        return ratio;
+    }
 }
--- src/aisd/zesp/ambulanceservices/geometry/Point.java
+++ src/aisd/zesp/ambulanceservices/geometry/Point.java
@@ -1,4 +1,32 @@
 package aisd.zesp.ambulanceservices.geometry;
 
 public class Point {
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(double x, Point start, Point end) {
+        this.x = start.getX() + (end.getX() - start.getX())/x;
+        y = start.getY() + (end.getY() - start.getY())/x;
+    }
+
+    public double getRelativeDirection(Point a, Point b){
+        return (b.getX() - a.getX())*(y - a.getY()) - (b.getY() - a.getY())*(x - a.getX());
+    }
+
+    public boolean isLeft(Point start, Point end){
+        return getRelativeDirection(start, end) > 0;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
