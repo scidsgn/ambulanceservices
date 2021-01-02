d0ac9220d6b8dc6ea544569632495232b933949f
Achreko <achrekv3@gmail.com>
2021-01-02 12:51:09 +0100

    It should be possible to intersect lines although tests will be added later today.

--- src/aisd/zesp/ambulanceservices/geometry/LineIntersection.java
+++ src/aisd/zesp/ambulanceservices/geometry/LineIntersection.java
@@ -1,10 +1,39 @@
 package aisd.zesp.ambulanceservices.geometry;
 
 public class LineIntersection {
-    public double intersect(Point start, Point end){
+
+    public double intersect(Point startFirst, Point endFirst, Point startSecond, Point endSecond){
         double ratio = 0;
+        double denominator = (startFirst.getX() - endFirst.getX())*(startSecond.getY() - endSecond.getY())
+                - (startFirst.getY() - endFirst.getY())*(startSecond.getX() - endSecond.getX());
 
+        if(denominator != 0) {
+            double t = getT(startFirst, endFirst, startSecond, endSecond);
+            double u = getU(startFirst, endFirst, startSecond, endSecond);
 
+            if(t >= 0 && t <= 1){
+                ratio = t;
+            }
+            else if(u >= 0 && u <= 1 ){
+                ratio = u;
+            }
+        }
         return ratio;
     }
+
+    public double getT(Point startFirst, Point endFirst, Point startSecond, Point endSecond){
+        return ((startFirst.getX() - startSecond.getX())*(startSecond.getY() - endSecond.getY())
+                - (startFirst.getY() - startSecond.getY())*(startSecond.getX() - endSecond.getX()))
+                / ((startFirst.getX() - endFirst.getX())*(startSecond.getY() - endSecond.getY())
+                - (startFirst.getY() - endFirst.getY())*(startSecond.getX() - endSecond.getX()) );
+    }
+
+    public double getU(Point startFirst, Point endFirst, Point startSecond, Point endSecond){
+        return -((startFirst.getX() - endFirst.getX())*(startFirst.getY() - startSecond.getY())
+                - (startFirst.getY() - endFirst.getY())*(startFirst.getX() - startSecond.getX()))
+                / ((startFirst.getX() - endFirst.getX())*(startSecond.getY() - endSecond.getY())
+                - (startFirst.getY() - endFirst.getY())*(startSecond.getX() - endSecond.getX()) );
+    }
 }
+
+
