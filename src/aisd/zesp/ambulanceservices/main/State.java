78724834c94be850745ca562c2546df1fcbdd880
scintilla4evr <kacper17@outlook.com>
2021-01-09 00:34:29 +0100

    Started fixing State methods

--- src/aisd/zesp/ambulanceservices/main/State.java
+++ src/aisd/zesp/ambulanceservices/main/State.java
@@ -1,28 +1,58 @@
 package aisd.zesp.ambulanceservices.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class State {
+    private final List<Hospital> hospitalList;
+    private final List<Patient> patientList;
     private int connectionNumber = 0;
 
+    public State() {
+        hospitalList = new ArrayList<>();
+        patientList = new ArrayList<>();
+    }
+
     public Hospital getHospitalById(int id) {
-        Hospital hospital = null;
+        for (Hospital hospital : hospitalList) {
+            if (hospital.getId() == id) {
+                return hospital;
+            }
+        }
 
-
-
-        return hospital;
+        return null;
     }
 
     public Patient getPatientById(int id) {
-        Patient patient = null;
+        for (Patient patient : patientList) {
+            if (patient.getId() == id) {
+                return patient;
+            }
+        }
 
-        return patient;
+        return null;
     }
 
     public void addPatient(Patient patient) {
+        if  (patient == null) {
+            throw new NullPointerException("Patient cannot be null.");
+        }
+        if (getPatientById(patient.getId()) != null) {
+            throw new IllegalArgumentException("Patient with that ID already added.");
+        }
 
+        patientList.add(patient);
     }
 
-    public void addHospital(Hospital hospital) {
+    public void addHospital(Hospital hospital) throws IllegalArgumentException, NullPointerException {
+        if  (hospital == null) {
+            throw new NullPointerException("Hospital cannot be null.");
+        }
+        if (getHospitalById(hospital.getId()) != null) {
+            throw new IllegalArgumentException("Hospital with that ID already added.");
+        }
 
+        hospitalList.add(hospital);
     }
 
     public Landmark getLandmarkById(int id) {
