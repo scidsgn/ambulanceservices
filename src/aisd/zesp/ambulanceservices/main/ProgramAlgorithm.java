c3e97c1b486f2f8414f3f3236e40d4ab1f1577f6
scintilla4evr <kacper17@outlook.com>
2021-01-09 21:39:05 +0100

    Started working on the full algorithm flow

--- src/aisd/zesp/ambulanceservices/main/ProgramAlgorithm.java
+++ src/aisd/zesp/ambulanceservices/main/ProgramAlgorithm.java
@@ -1,8 +1,36 @@
 package aisd.zesp.ambulanceservices.main;
 
+import aisd.zesp.ambulanceservices.graham.ConvexHull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProgramAlgorithm {
     private State state = null;
     private Patient currentPatient;
+    private List<Hospital> visitedHospitals;
+    private Hospital currentHospital;
+
+    private void selectPatient(Patient patient) {
+        currentPatient = patient;
+        visitedHospitals = new ArrayList<>();
+        currentHospital = null;
+    }
+
+    private void goToFirstHospital() {
+        // todo
+    }
+
+    private void goToNextHospital() {
+        Hospital nextHospital = state.getNextHospital(currentHospital);
+
+        if (nextHospital.getVacantBeds() > 0) {
+            visitedHospitals.add(nextHospital);
+            currentHospital = nextHospital;
+        } else {
+            currentPatient.setPatientState(PatientState.ABANDONED);
+        }
+    }
 
     public State getState() {
         return state;
