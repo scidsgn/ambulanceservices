78465f6bbdabef33d074cf77340913339fe54d72
scintilla4evr <kacper17@outlook.com>
2021-01-14 17:38:31 +0100

    Improved path length display on the map

--- src/aisd/zesp/ambulanceservices/screen/MapCanvas.java
+++ src/aisd/zesp/ambulanceservices/screen/MapCanvas.java
@@ -8,6 +8,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 import java.util.List;
 
@@ -106,6 +107,28 @@ public class MapCanvas extends Canvas {
         g.stroke();
     }
 
+    private double[] measureText(String str) {
+        Text text = new Text(str);
+        double[] dims = new double[2];
+
+        dims[0] = text.getLayoutBounds().getWidth();
+        dims[1] = text.getLayoutBounds().getHeight();
+
+        return dims;
+    }
+
+    private void drawLength(GraphicsContext g, Point p1, Point p2, double length) {
+        Point middle = new Point(0.5, p1, p2);
+        String lengthText = String.valueOf(Math.round(length));
+
+        double[] textBounds = measureText(lengthText);
+
+        g.setFill(Color.ORANGE);
+        g.fillRect(middle.getX() - textBounds[0] / 2 - 8, middle.getY() - textBounds[1] / 2, textBounds[0] + 16, textBounds[1]);
+        g.setFill(Color.BLACK);
+        g.fillText(lengthText, middle.getX() - textBounds[0] / 2, middle.getY() + 4);
+    }
+
     private void drawPaths(GraphicsContext g) {
         Graph<Point> graph = programAlgorithm.getState().getConnectionsGraph();
         List<Point> graphNodes = graph.getNodes();
@@ -121,19 +144,35 @@ public class MapCanvas extends Canvas {
                 if (length != null) {
                     Point screenPoint1 = worldToCanvas(p1);
                     Point screenPoint2 = worldToCanvas(p2);
-                    Point screenPathMiddle = new Point(0.5, screenPoint1, screenPoint2);
 
                     g.moveTo(screenPoint1.getX(), screenPoint1.getY());
                     g.lineTo(screenPoint2.getX(), screenPoint2.getY());
-
-                    g.fillText("L: " + length, screenPathMiddle.getX() + 5, screenPathMiddle.getY() + 5);
                 }
             }
         }
-
         g.setStroke(graphEdgeStroke);
         g.setLineWidth(4);
         g.stroke();
+
+        for (int i = 0; i < graphNodes.size(); i++) {
+            for (int j = i + 1; j < graphNodes.size(); j++) {
+                Point p1 = graphNodes.get(i);
+                Point p2 = graphNodes.get(j);
+                Double length = graph.getLength(p1, p2);
+
+                if (length != null) {
+                    Point screenPoint1 = worldToCanvas(p1);
+                    Point screenPoint2 = worldToCanvas(p2);
+
+                    g.setFill(Color.BLACK);
+                    g.fillRect(screenPoint1.getX() - 3, screenPoint1.getY() - 3, 7, 7);
+                    g.setFill(Color.WHITE);
+                    g.fillRect(screenPoint1.getX() - 2, screenPoint1.getY() - 2, 5, 5);
+
+                    drawLength(g, screenPoint1, screenPoint2, length);
+                }
+            }
+        }
     }
 
     private void drawHospitals(GraphicsContext g) {
